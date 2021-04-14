da7727d8d9405a4d3e726723c59fade31f83a153
slatalison <xiaos157@newschool.edu>
2021-04-14 00:09:30 -0400

    fake完成

--- Assets/Scripts/Cards/CardInventory.cs
+++ Assets/Scripts/Cards/CardInventory.cs
@@ -9,11 +9,12 @@ public class CardInventory
     {
         cardItemList = new List<CardItem>();
 
-        AddCardItem(new CardItem { cardItemType = CardItemType.Card1, CardLevel = 1, name = "Defalut config", pressure = 90f, temperature = 10f, description = "Nothing happens. Tamas no longer produce Light Manna, nor dare they attack each other. What can be even worse than the dreadful boredness?", cost = 20 });
+        AddCardItem(new CardItem { cardItemType = CardItemType.Card1, CardLevel = 1, name = "Defalut config", pressure = 90f, temperature = 10f, description = "Nothing happens. Tamas no longer produce Light Manna, nor dare they attack each other. What can be even worse than the dreadful boredness?", cost = 10 });
         AddCardItem(new CardItem { cardItemType = CardItemType.Card2, CardLevel = 1, name = "Calm", pressure = 70f, temperature = 25f, description = "Not too warm and not too free. Maybe a little bit boring too, but at least there is little violence.", cost = 10 });
-        AddCardItem(new CardItem { cardItemType = CardItemType.Card3, CardLevel = 2, name = "Booming", pressure = 30f, temperature = 50f, description = "The community has not got the regulation and attention from the outside world, equals you. It grows at an incredible but also alarming speed.", cost = 30 });
-        AddCardItem(new CardItem { cardItemType = CardItemType.Card4, CardLevel = 3, name = "Agenda driven", pressure = 85f, temperature = 85f, description = "Poor Tamas are now being used as laborers! They are pushed by the harsh config to make no trouble but produce Light Manna.", cost = 20 });
-        AddCardItem(new CardItem { cardItemType = CardItemType.Card5, CardLevel = 3, name = "Political correctness", pressure = 75f, temperature = 65f, description = "Every Tama wants its voice to be heard. But if you listen, their voices are quite similar to each other. Perhaps, this is because there are too many You-Know-Whos in this petri dish.", cost = 20 });
+        AddCardItem(new CardItem { cardItemType = CardItemType.Card5, CardLevel = 2, name = "The Law of the jungle", pressure = 20f, temperature = 70f, description = "The nastiest survives because it has nothing to lose. For the nice and hardworking ones, on the other hand, any little disturbance is unbearable.", cost = 35 });
+        AddCardItem(new CardItem { cardItemType = CardItemType.Card3, CardLevel = 2, name = "Booming", pressure = 30f, temperature = 50f, description = "The community has not got much regulation and attention from the outside world, equals you. It grows at an incredible but also alarming speed.", cost = 45 });
+        AddCardItem(new CardItem { cardItemType = CardItemType.Card4, CardLevel = 3, name = "Agenda driven", pressure = 85f, temperature = 85f, description = "Poor Tamas are now being used as laborers! They are pushed by the harsh config to make no trouble but produce Light Manna.", cost = 100 });
+        AddCardItem(new CardItem { cardItemType = CardItemType.Card6, CardLevel = 4, name = "Political over-correctness", pressure = 75f, temperature = 65f, description = "Every Tama wants its voice to be heard. But if you listen, their voices are quite similar to each other. Perhaps, this is because there are too many You-Know-Whos in this petri dish.", cost = 220 });
 
     }
 
--- Assets/Scripts/UI&Visual/UI_TamaPanel.cs
+++ Assets/Scripts/UI&Visual/UI_TamaPanel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UI_TamaPanel : MonoBehaviour
@@ -8,6 +9,7 @@ public class UI_TamaPanel : MonoBehaviour
     private UI_TamaPanel tamaPanel;
     [SerializeField] private Transform cellContainer;
     [SerializeField] private Transform cellTemplate;
+    [SerializeField] private Transform TamaSelected;
     [SerializeField] float itemSlotCellSize = 400f;
     int x = 0;
     int y = 0;
@@ -32,6 +34,13 @@ public class UI_TamaPanel : MonoBehaviour
         ResetTabs();
         button.background.sprite = tabActive;
         _audio.PlayOneShot(openTabSound);
+
+        TamaSelected.Find("TamaSprite").GetComponent<Image>().sprite = button.transform.Find("TamaSprite").GetComponent<Image>().sprite;
+        TamaSelected.Find("TamaName").GetComponent<TextMeshProUGUI>().text = "Killer Tama";
+        TamaSelected.Find("Description").GetComponent<TextMeshProUGUI>().text = "Killer Tamas are trained well to be efficient vigilantes, or if you ask their enemies, psychopath serial killers.";
+        TamaSelected.Find("rare").GetComponent<TextMeshProUGUI>().text = "4";
+        TamaSelected.Find("TagPres/PresTagText").GetComponent<TextMeshProUGUI>().text = "Orderless";
+
     }
 
     public void ResetTabs()
@@ -52,7 +61,7 @@ public class UI_TamaPanel : MonoBehaviour
     public void SetTamaCollection(UI_TamaPanel tamaPanel)
     {
         this.tamaPanel = tamaPanel;
-        RefreshInventoryItems();
+        //RefreshInventoryItems();
     }
 
     private void RefreshInventoryItems()
@@ -65,6 +74,7 @@ public class UI_TamaPanel : MonoBehaviour
             tamaCellItem.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
         }
     }
+
 
 
 }
